b0ace154e1105688da8302b398418b96331b1ff3
AlphaEliasPY <84801328+AlphaEliasPY@users.noreply.github.com>
2021-07-08 13:35:18 -0400

    Update string_gen.py

--- string_gen.py
+++ string_gen.py
@@ -1,31 +1,50 @@
 #!/usr/bin/env python3
-# (c) https://t.me/TelethonChat/37677
 # This Source Code Form is subject to the terms of the GNU
 # General Public License, v.3.0. If a copy of the GPL was not distributed with this
 # file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.en.html.
 
-try:
-    from telethon.sessions import StringSession
-    from telethon.sync import TelegramClient
-except BaseException:
-    print("Telethon Not Found. Installing Now.")
-    import os
+print("an online StringSession generator")
 
-    os.system("pip3 install telethon")
-    from telethon.sessions import StringSession
-    from telethon.sync import TelegramClient
-ok = """ ____  ____  __  ____   __   _  _
-Thunder
-"""
-print(ok)
-APP_ID = int(input("Enter APP ID here: \n"))
-API_HASH = input("Enter API HASH here: \n")
+print("p ==> Pyrogram (docs.pyrogram.org)")
+print("PyroGramUserBot ==> https://GitHub.com/SpEcHiDe/PyroGramUserBot")
 
-client = TelegramClient(StringSession(), APP_ID, API_HASH)
-with client:
+print("t ==> Telethon (docs.telethon.dev)")
+print("Telethon UserBot ==> https://GitHub.com/SpEcHiDe/UniBorg")
+
+print("Select your required option: ")
+s_l = input("enter p / t ? ?? ")
+
+if s_l == "p":
+  print("you selected Pyrogram")
+  APP_ID = int(input("Enter APP ID here: "))
+  API_HASH = input("Enter API HASH here: ")
+  import pyrogram
+  with pyrogram.Client(
+    ":memory:",
+    api_id=APP_ID,
+    api_hash=API_HASH
+  ) as app:
+    session_str = app.export_session_string()
+    s_m = app.send_message("me", session_str)
+    s_m.reply_text("⬆️ This StringSession is generated using https://GenerateStringSession.SpEcHIDe.repl.run \nPlease subscribe @UniBorg ", quote=True)
+    print("please check your Telegram Saved Messages for the StringSession ")
+
+elif s_l == "t":
+  print("you selected Telethon")
+  # (c) https://t.me/TelethonChat/37677
+  from telethon.sync import TelegramClient
+  from telethon.sessions import StringSession
+  APP_ID = int(input("Enter APP ID here: "))
+  API_HASH = input("Enter API HASH here: ")
+  with TelegramClient(
+    StringSession(), 
+    APP_ID, 
+    API_HASH
+  ) as client:
     session_str = client.session.save()
-    client.send_message("me", f"`{session_str}`")
-    client.send_message(
-        "THIS IS YOUR STRING SESSION \nJoin @AndroidCave For More Support."
-    )
-    print("⬆ Please Check Your Telegram Saved Message For Your String.")
+    s_m = client.send_message("me", session_str)
+    s_m.reply("⬆️ This StringSession is generated using https://GenerateStringSession.SpEcHIDe.repl.run! \nPlease subscribe @AndroidCave ")
+    print("please check your Telegram Saved Messages for the StringSession ")
+
+else:
+  print("?? please select only p / t, ")
